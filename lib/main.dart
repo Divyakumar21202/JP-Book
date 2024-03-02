255cf34c1817a9eb35a56e3e0afc6a6be0cf420a
= <b122079@iiit-bh.ac.in>
2024-03-02 16:53:17 +0530

    Added Bottom NavBar

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<BottomNavigationBarItem> _bottomNavigationBarItemList = const [
+    BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Parties'),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.my_library_books_rounded), label: 'Bills'),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.card_giftcard_rounded), label: 'Items'),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.business_center_rounded), label: 'Loans'),
+    BottomNavigationBarItem(icon: Icon(Icons.menu_rounded), label: 'More'),
+  ];
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.book),
+        title: const Text(
+          'My Business',
+          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+        ),
+        actions: const [Icon(Icons.perm_contact_calendar), SizedBox(width: 8)],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.blue,
+        items: _bottomNavigationBarItemList,
+        currentIndex: _currentIndex,
+        onTap: (int? val) {
+          _currentIndex = val!;
+          setState(() {});
+        },
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
